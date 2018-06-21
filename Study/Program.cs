f939dbc25b0a81c5c3476e9be63172c137b4d307
admin <824569950@qq.com>
2018-06-21 14:28:57 +0800

    更改测试

--- Study/Program.cs
+++ Study/Program.cs
@@ -154,6 +154,7 @@ namespace Study
             int j = (int)o;
             //判断类型是否相同
             bool boo = o is Object;
+            string str = "git check";
             Console.WriteLine(i.GetType() == o.GetType());
             Console.ReadKey();
         }
